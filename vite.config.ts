107f55db774637be9f9d99774ceb8e33e9a22458
sjhanamoto-droid <sjhanamoto@gmail.com>
2026-01-04 20:28:10 +0900

    レポジトリ

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+    base: '/first-class_LP/',
     server: {
         port: 3000,
         host: '0.0.0.0',
